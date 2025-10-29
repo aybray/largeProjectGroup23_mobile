e7d27fd9d17dfcd35dacbf27b99f0726dd940c15
ccm89 <chris.colon.marq@gmail.com>
2025-10-29 09:22:18 -0400

    update: Gradle and Android Manifest with internet permission and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -58,4 +59,12 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // Networking Dependencies
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+    // implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    // implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+    // implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
